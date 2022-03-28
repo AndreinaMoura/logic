74ef4b67c23792ccf00986fff963e1e877004aab
AndreinaMoura <andreina.moura3004@gmail.com>
2022-03-28 11:26:18 -0300

    Projeto

--- FPOO/wk/workspace/Pa/src/exercicios/Ex6.java
+++ FPOO/wk/workspace/Pa/src/exercicios/Ex6.java
@@ -7,41 +7,50 @@ public class Ex6 {
 
 	public static void main(String[] args) {
 		// 6 Faça um programa para gerar automaticamente numeros entre 0 e 99 de uma
-		// cartela de bingo.
-		// Sabendo que cada cartela devera conter 5 linhas de 5 números, gere estes
-		// dados de modo a nao ter
-		// números repetidos dentro das cartelas. O programa deve exibir na tela a
-		// cartela gerada
+		// cartela de bingo. Sabendo que cada cartela devera conter 5 linhas de 5
+		// números, gere estes dados de modo a nao ter números repetidos dentro das
+		// cartelas. O programa deve exibir na tela a cartela gerada
 
 		int matriz[][] = new int[5][5];
+		int vetor[] = new int[25];
 
 		System.out.println("Cartela de bingo 5x5");
+		
+		for (int i = 0; i < 25; i++) {
+			vetor[i] = r.nextInt(25);
+		}
+		
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				matriz[i][j] = r.nextInt(100);
+				int x = numerosAleatorios(vetor);
+				matriz[i][j] = x;
 			}
 		}
-		boolean repetido = false;
-		while (repetido != true) {
-			for (int i = 0; i < 5; i++) {
-				for (int j = 0; j < 5; j++) {
-					for (int k = 0; k < 5; k++) {
-						for (int l = 0; l < 5; l++) {
-							if (matriz[i][j] == matriz[k][l]) {
-								matriz[i][j] = r.nextInt(100);
-							}else{
-								repetido=true;
-							}
-						}
-					}
-				}
-			}
-		} 
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				System.out.print(matriz[i][j] + "\t");
 			}
 			System.out.println();
 		}
+	}
+
+	private static int numerosAleatorios(int[] vetor) {
+		boolean contem = true;
+		int numeroAleatório = 0; 
+		while (contem) {
+			contem=false;
+			for (int i = 0; i < 25; i++) {
+					if (vetor[i] == vetor[i]) {
+						contem=true;
+						break;
+					
+					if(contem) {
+						numeroAleatório = r.nextInt(25);
+						return numeroAleatório;
+					}
+					}
+				}
+			}
+		return numeroAleatório;
 	}
 }
