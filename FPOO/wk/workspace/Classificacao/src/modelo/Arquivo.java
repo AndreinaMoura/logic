f51d7a3cf7e309139a62f72bfd841799157952ca
AndreinaMoura <andreina.moura3004@gmail.com>
2022-04-26 11:10:54 -0300

    Projeto

---
+++ FPOO/wk/workspace/Classificacao/src/modelo/Arquivo.java
@@ -0,0 +1,47 @@
+package modelo;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Arquivo {
+//	1 - Em um arquivo de texto, armazene o nome e o ano de nascimento das pessoas.
+//	Ao ler esse arquivo o sistema irá informar o nome e a idade.
+	public void cadastrar(String info, String fileName, boolean append) {
+
+		
+		
+		try {
+			FileWriter fw = new FileWriter(fileName + ".csv", append);
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			bw.write(info + "\r\n");
+
+			bw.close();
+			fw.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	public ArrayList<String> ler(String fileName) {
+		ArrayList<String> data = new ArrayList<>();
+		try {
+			FileReader fr = new FileReader(fileName + ".csv");
+			BufferedReader br = new BufferedReader(fr);
+
+			String linha = "";
+			while ((linha = br.readLine()) != null) {
+				data.add(linha);
+			}
+			br.close();
+			fr.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return data;
+	}
+}
---
+++ FPOO/wk/workspace/Solucao/src/vsao/Tela.java
@@ -0,0 +1,22 @@
+package vsao;
+
+import java.util.Scanner;
+
+import controle.ProcessaPlanilha;
+
+public class Tela {
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int menu = 0;
+		ProcessaPlanilha.carregar();
+		
+		while (menu != 5) {
+			System.out.println("1.Adição\n2.Multiplicação\n3.Subtração\n4.Divisão\n5.Sair\n");
+			menu = scan.nextInt();
+			
+			ProcessaPlanilha.saida(menu);	
+		}
+	}
+}
